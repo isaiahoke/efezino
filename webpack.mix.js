7535227aab9427aacc1ef6f8361f8038fb49c6cf
ISAIAH O A <damanagerltd@gmail.com>
2021-11-22 01:11:29 +0000

    browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
+   .postCss('resources/css/app.css', 'public/css', [
+    //.postCss('resources/css/main.css', 'public/css', [
+        require("tailwindcss"),
     ]);
+    
+mix.browserSync('efezino.test');
+
+mix.browserSync({
+    proxy: "http://localhost:8000"
+});
